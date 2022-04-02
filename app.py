6bf44e2d0bb3656c890c4e4144751df7ee574ce7
EslamSallam <eslam.salam1369@gmail.com>
2022-04-02 20:55:30 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, Path, HTTPException, status
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
+
+# simple Get root Function
+@app.get("/")
+def home():
+    return {"Data": "Test"}
+
+# simple Get About function
+@app.get("/about")
+def about():
+    return {"Data": "About"}
+
+inventory = {
+    1: {
+        "name": "Milk",
+        "price": 3.99,
+        "brand": "Regular"
+    }
+}
+
+# Path Params Example
+@app.get("/get-item/{item_id}/{name}")
+def get_item(item_id: int = Path(None, description="The Id of the item you would like to view", gt=0, lt=2), name: str = None):
+    return inventory[item_id]
+
+# Query params Example
+# passing test case: http://127.0.0.1:8000/get-by-name/1?name=Milk&test=1
+@app.get("/get-by-name/{item_id}")
+def get_item(*, item_id: int, name: Optional[str] = None, test: int):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item name not found")
+
+# simple post request implementation
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    for id in inventory:
+        if item_id == id:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Item id already Added")
+    inventory[item_id] = item
+    return inventory[item_id]
